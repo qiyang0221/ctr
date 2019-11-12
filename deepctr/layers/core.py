8da26e7b0faa18f63241a6054427bdd08c64843d
qiyang0221 <qiyang0221@gmail.com>
2019-11-11 23:43:02 -0800

    Update core.py
    add DNN function

--- deepctr/layers/core.py
+++ deepctr/layers/core.py
@@ -25,14 +25,14 @@ class DNN(Layer):
             - **seed**: random seed
     '''
 
-    def __init__(self, hidden_units, activation='relu', l2=0, dropout_rate=0, use_bn=False, seed=1024, *args, **kwargs):
+    def __init__(self, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False, seed=1024, **kwargs):
         self.hidden_units = hidden_units
         self.activation = activation
         self.dropout_rate = dropout_rate
         self.seed = seed
-        self.l2 = l2
+        self.l2_reg = l2_reg
         self.use_bn = use_bn
-        super().__init__(*args, **kwargs)
+        super().__init__(**kwargs)
     
     def build(self, input_shape):
         input_size = input_shape[-1]
@@ -42,5 +42,48 @@ class DNN(Layer):
                                             hidden_units[i], hidden_units[i + 1]),
                                         initializer=glorot_normal(
                                             seed=self.seed),
-                                        regularizer=l2(self.l2),
+                                        regularizer=l2(self.l2_reg),
                                         trainable=True) for i in range(len(self.hidden_units))]
+        self.bias = [self.add_weight(name='bias' + str(i),
+                                     shape=(self.hidden_units[i],),
+                                     initializer=Zeros(),
+                                     trainable=True) for i in range(len(self.hidden_units))]
+        if self.use_bn:
+            self.bn_layers = [tf.keras.layers.BatchNormalization() for _ in range(len(self.hidden_units))]
+        
+        self.dropout_layers = [tf.keras.layers.Dropout(self.dropout_rate, seed=self.seed+i) for i in range(len(self.hidden_units))]
+        self.activation_layers = [activation_layer(self.activation) for _ in range(len(self.hidden_units))]
+
+        super(DNN, self).build(input_shape)
+    
+    def call(self, inputs, training=None, **kwargs):
+
+        deep_input = inputs
+
+        for i in range(len(self.hidden_units)):
+            fc = tf.nn.bias_add(tf.tensordot(
+                deep_input, self.kernels[i], axes=(-1, 0)), self.bias[i])
+            if self.use_bn:
+                fc = self.bn_layers[i](fc, training=trianing)
+            
+            fc = self.activation_layers[i](fc)
+
+            fc = self.dropout_layers[i](fc, training = training)
+            deep_input = fc
+        
+        return deep_input
+    
+    def compute_output_shape(self, input_shape):
+        if len(self.hidden_units) > 0:
+            shape = input_shape[:-1] + (self.hidden_units[-1],)
+        else:
+            shape = input_shape
+        
+        return tuple(shape)
+    
+    def get_config(self,):
+        config = {'activation': self.activation, 'hidden_units': self.hidden_units,
+                  'l2_reg': self.l2_reg, 'use_bn': self.use_bn, 'dropout_rate': self.dropout_rate, 'seed': self.seed}
+        base_config = super(DNN, self).get_config()
+
+        return dict(list(base_config.items()) + list(config.items()))
